484bbdcdb563fb99777d02c7a930d3af33756f6f
chan-fun <chan19900217@gmail.com>
2025-05-30 14:32:54 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,7 @@ import os
 
 # 目标URL列表
 urls = [ 'https://ip.164746.xyz',
-        'https://cf.090227.xyz/'
+        'https://cf.090227.xyz'
         ]
 
 # 正则表达式用于匹配IP地址
@@ -25,7 +25,7 @@ with open('ip.txt', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://cf.090227.xyz/':
+        if url == 'https://cf.090227.xyz':
             elements = soup.find_all('tr')
         elif url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
